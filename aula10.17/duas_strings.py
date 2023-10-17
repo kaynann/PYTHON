842da1a4b4b3d4c0c4b2c1396a230c7abcbb564b
kaynann <kaynanjunior098@gmail.com>
2023-10-17 16:42:56 -0300

    Utilizando SCAPE nas variaveis

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,8 +1,12 @@
+# você pode ESCAPAR aspas dulaps usando aspas simples
+# vale ressaltar que isso só vale para ASPAS DUPLAS
+
 # texto1 recebe o valor "Sou do"
 texto1 = "Sou do"
 
 # texto2 rece o valor "back end
-texto2 = "back end"
+# caracter de SCAPE \
+texto2 = "\"back end\""
 
 # a função print concatena e imprime as duas variáveis na tela
 print(texto1, texto2)

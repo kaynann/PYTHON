90351b8110c75eb5d6afe7a023f3481a3deba2a0
kaynann <kaynanjunior098@gmail.com>
2023-10-18 16:08:29 -0300

    modificações no código do desafio02.py

--- aula10.18/desafio02.py
+++ aula10.18/desafio02.py
@@ -1,7 +1,6 @@
 user_year = input("Informe sua idade: ")
 user_year_parent = input("Informe a idade de qualquer parente seu(mãe, pai, irmão): ")
 user_name = input("Informe seu nome: ")
-multiplica_idades =  int(user_year) * int(user_year_parent)
 
-print(multiplica_idades)
+print(int(user_year) * int(user_year_parent))
 print(user_name)

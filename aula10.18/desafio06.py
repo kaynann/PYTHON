005acace4a8834f0f7fbc6c6dc74359a6dd3972d
kaynann <kaynanjunior098@gmail.com>
2023-10-19 15:23:59 -0300

    modificações nos arquivos do desafio

--- aula10.18/desafio06.py
+++ aula10.18/desafio06.py
@@ -1,4 +1,4 @@
 base = input("Informe a base: ")
 altura = input("Informe a altura: ")
 area = int(base) * int(altura) 
-print(area * 2)
+print("A área é: ",area * 2)
--- aula10.18/desafio07.py
+++ aula10.18/desafio07.py
@@ -4,7 +4,7 @@ number2 = input("Informe o segundo número inteiro: ")
 number3 = input("Informe o terceiro número inteiro: ")
 
 # execução da primeiro cálculo pedido na questão
-product = (int(number1) * 2) + (int(number2) / 2) + int(number3)
+product = ((int(number1) * 2) * (int(number2) / 2)) + int(number3)
 print(int(product))
 
 # execução do segundo cálculo pedido na questão

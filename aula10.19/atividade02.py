a25da25210fb9430d611e52fe6dbd4a3856ec6b3
kaynann <kaynanjunior098@gmail.com>
2023-10-19 17:30:29 -0300

    adicionado o arquivo atividade02.py da aula

---
+++ aula10.19/atividade02.py
@@ -0,0 +1,7 @@
+user_height = input("Qual sua altura em metros? ")
+weight_man = (72.7 * float(user_height)) -58
+weight_girl = (62.1 * float(user_height)) - 44.7
+
+print("Se você for homem, seu peso ideal é:", float(weight_man),"Kg")
+print("Se você for mulher, seu peso ideal é:", float(weight_girl),"Kg")
+

eff03296e5bb979041f63dd052c6a86f7a9a1f5e
kaynann <kaynanjunior098@gmail.com>
2023-10-20 14:40:50 -0300

    Fazendo melhorias no código do arquivo atividade02.py

--- aula10.19/atividade02.py
+++ aula10.19/atividade02.py
@@ -1,6 +1,6 @@
-user_height = input("Qual sua altura em metros? ")
+user_height = input("Informe sua altura em metros(Ex: 1.50): ")
 weight_man = (72.7 * float(user_height)) -58
 weight_girl = (62.1 * float(user_height)) - 44.7
 
-print("Se você for homem, seu peso ideal é:", float(weight_man),"Kg")
-print("Se você for mulher, seu peso ideal é:", float(weight_girl),"Kg")
+print(f'Se você for homem, seu peso ideal é: {float(weight_man)} Kg')
+print(f'Se você for mulher, seu peso ideal é: {float(weight_girl)} Kg')

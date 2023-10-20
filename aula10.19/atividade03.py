10c0aa4777a1ce7eddf453bdff5b16f2bea37182
kaynann <kaynanjunior098@gmail.com>
2023-10-20 14:56:17 -0300

    adicionando o arquivo atividade02.py

---
+++ aula10.19/atividade03.py
@@ -0,0 +1,9 @@
+# variáveis de input recebendo um valor inserido pelo usuário
+salario = float(input("Informe seu salário atual: "))
+reajuste = float(input("Informe a porcentagem do reajuste: "))
+
+# variáveis de que calculam a porcentagem do salario e o novo salário do usuário
+valor_aumento = salario * reajuste / 100
+novo_salario = salario + valor_aumento
+
+print(f'O seu novo salário é R$ {novo_salario}')

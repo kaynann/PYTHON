5015ef2591e6c2b4a97168a879a891feafdbde78
kaynann <kaynanjunior098@gmail.com>
2023-10-20 14:55:36 -0300

    adicionado comentários explicando a execução do código no arquivo atividade02.py

--- aula10.19/atividade02.py
+++ aula10.19/atividade02.py
@@ -1,4 +1,7 @@
+# variavel com input pedindo um valor para o usuário
 user_height = input("Informe sua altura em metros(Ex: 1.50): ")
+
+# variáveis que calculam o peso ideal com base no valor inserido pelo usuário
 weight_man = (72.7 * float(user_height)) -58
 weight_girl = (62.1 * float(user_height)) - 44.7
 

bc69b5bbf60b8d42a96bfd7240b48fd43f492f6c
kaynann <kaynanjunior098@gmail.com>
2023-11-06 16:35:37 -0300

    Inciando estudo com maquina virtual na aula 06/11

---
+++ aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,7 @@
+# variaveis são usadas para salvar algo na memória do computador
+# o sinal de = é utilizado para atribuição e valores
+
+var01 = 'alguma coisa'
+print(var01)
+var01 = 'outra coisa'
+print(var01)

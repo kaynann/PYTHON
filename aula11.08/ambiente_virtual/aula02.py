9ebdeb9c23925e2c622c56488cc88754e53d8cc3
kaynann <kaynanjunior098@gmail.com>
2023-11-08 14:48:12 -0300

    Adicionado arquivo02.py praticando estruturas condicionais if e elif

---
+++ aula11.08/ambiente_virtual/aula02.py
@@ -0,0 +1,16 @@
+entrada = input('[E] para entra e [S] para passar: ')
+senha_digitada = input('Digite a senha de acesso: ')
+senha = '12345678'
+
+if (entrada == 'E' or entrada == 'e'):
+  if (senha == senha_digitada):
+    print('Sucesso, você entrou')
+  else:
+    print('Senha incorreta')
+elif(entrada == 'S' or entrada == 's'):
+  if(senha == senha_digitada):
+    print('Sucesso, você passou')
+  else:
+    print('Senha incorreta')
+else:
+  print('Você não selecionou uma opção válida')  

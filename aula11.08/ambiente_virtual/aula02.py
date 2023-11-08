5e633cc0323637a5cff61d0468737b38f8b43adf
kaynann <kaynanjunior098@gmail.com>
2023-11-08 14:50:52 -0300

    Melhorando o código

--- aula11.08/ambiente_virtual/aula02.py
+++ aula11.08/ambiente_virtual/aula02.py
@@ -4,13 +4,13 @@ senha = '12345678'
 
 if (entrada == 'E' or entrada == 'e'):
   if (senha == senha_digitada):
-    print('Sucesso, você entrou')
+    print("Sucesso, você entrou")
   else:
-    print('Senha incorreta')
+    print("Senha incorreta")
 elif(entrada == 'S' or entrada == 's'):
   if(senha == senha_digitada):
-    print('Sucesso, você passou')
+    print("Sucesso, você passou")
   else:
-    print('Senha incorreta')
+    print("Senha incorreta")
 else:
-  print('Você não selecionou uma opção válida')  
+  print("Você não selecionou uma opção válida")  

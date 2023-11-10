2c71074cbf424b120617ccc05ddf6f67790f40cb
kaynann <kaynanjunior098@gmail.com>
2023-11-10 14:04:53 -0300

    Aividade trabalhando com arrays

---
+++ aula11.09/ambiente_virtual/atividade01.py
@@ -0,0 +1,12 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+print(texto[0:5])
+print(texto[10:17])
+print(texto[20:30])
+print(texto[34:44])
+print(texto[67:74])
+print(texto[101:109])
+print(texto[126:133])
+print(texto[172:185])
+print(texto[186:191])
+print(texto[-10:-1])
---
+++ aula11.09/atividade02.py
@@ -0,0 +1,12 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+p1 = texto[6:10]
+p2 = texto[10:17]
+p3 = texto[49:53]
+p4 = texto[76:88]
+p5 = texto[157:159]
+p6 = texto[160:171]
+p7 = texto[186:191]
+p8 = texto[67:74]
+
+print(p1, p2, p3, p4, p5, p6, p7, p8)

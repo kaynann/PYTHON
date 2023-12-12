b7e31599e551ddb7d6c5681b70cf292962f01519
kaynann <kaynanjunior098@gmail.com>
2023-12-12 17:15:17 -0300

    alterações

--- aula11.22/atv04.py
+++ aula11.22/atv04.py
@@ -10,7 +10,7 @@ adult = sum(ages) / len(ages)
 elderly = sum(ages) / len(ages)
 
 if young >= 0 and young <= 25:
-  print(f'Turma novem')
+  print(f'Turma jovem')
 elif adult >= 26 and adult <= 60:
   print('Turma adulta')
 elif elderly > 60:
